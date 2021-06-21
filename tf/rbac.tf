8987df8680d908fece554f52ca52467412c9fac6
Veronica Petersen <veronicap@fairwaymc.com>
2021-06-21 12:17:49 -0500

    update to service-name

--- tf/rbac.tf
+++ tf/rbac.tf
@@ -1,11 +1,11 @@
 ﻿resource "azurerm_role_assignment" "ACM_Global" {
   scope                = local.settings.acmglobal
   role_definition_name = "App Configuration Data Reader"
-  principal_id         = azurerm_function_app.valuetrac.identity.0.principal_id
+  principal_id         = azurerm_function_app.service-name.identity.0.principal_id
 }
 
 resource "azurerm_role_assignment" "ACM_Global_Secondary" {
   scope                = local.settings.acmglobalsecondary
   role_definition_name = "App Configuration Data Reader"
-  principal_id         = azurerm_function_app.valuetrac.identity.0.principal_id
+  principal_id         = azurerm_function_app.service-name.identity.0.principal_id
 }
