fe59072441d1a9500bf67a9e6c8ded6b4073f6a7
Veronica Petersen <veronicap@fairwaymc.com>
2021-08-05 13:43:18 -0500

    add logging

---
+++ tf/logging.tf
@@ -0,0 +1,29 @@
+﻿data "azurerm_log_analytics_workspace" "main" {
+  name                = local.settings.log_analytics
+  resource_group_name = local.settings.log_analytics_rg
+}
+
+resource "azurerm_monitor_diagnostic_setting" "keyvault_monitoring" {
+  name                       = "LogAnalytics-Diagnostics"
+  target_resource_id         = azurerm_key_vault.keyVault.id
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.main.id
+
+  log {
+    category = "AuditEvent"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+}
