73e04c6daf15772282a7d78e770ebfa1da7a9f34
lokeshm <lokeshm@fairwaymc.com>
2021-09-10 12:02:40 -0500

    refactored base error models, updated axios version

--- code/shared/utils/customLoggerImpl.service.ts
+++ code/shared/utils/customLoggerImpl.service.ts
@@ -7,109 +7,89 @@ import { SeverityLevel } from "applicationinsights/out/Declarations/Contracts";
 import { BaseError } from "../model/baseError.model";
 import { classToPlain } from "class-transformer";
 
+
 @injectable()
-export class CustomLoggerImpl implements CustomLogger {
-    private readonly env = process.env["environment"];
-    private readonly client: appInsights.TelemetryClient = appInsights.defaultClient;
-    private readonly LogLevelMap: Map<string, number[]> = new Map([
-        [
-            "DEBUG",
-            [
-                SeverityLevel.Verbose,
-                SeverityLevel.Information,
-                SeverityLevel.Warning,
-                SeverityLevel.Error,
-                SeverityLevel.Critical,
-            ],
-        ],
-        ["INFO", [SeverityLevel.Information, SeverityLevel.Warning, SeverityLevel.Error, SeverityLevel.Critical]],
-        ["WARN", [SeverityLevel.Warning, SeverityLevel.Error, SeverityLevel.Critical]],
-        ["ERROR", [SeverityLevel.Error, SeverityLevel.Critical]],
-    ]);
-
-    /**
-     * For logging information for testing purposes only. Will not log in Production envirnonment.
-     * @param message String of information to log.
-     */
-    trace(message: string): void {
-        this.logMessage(message, SeverityLevel.Verbose);
+ export class CustomLoggerImpl implements CustomLogger{
+      private readonly env=process.env["environment"];
+      private readonly client: appInsights.TelemetryClient = appInsights.defaultClient;
+      private readonly LogLevelMap:Map<string,number[]>=new Map([
+        ["DEBUG",[SeverityLevel.Verbose,SeverityLevel.Information,SeverityLevel.Warning,SeverityLevel.Error,SeverityLevel.Critical]],
+        ["INFO",[SeverityLevel.Information,SeverityLevel.Warning,SeverityLevel.Error,SeverityLevel.Critical]],
+        ["WARN",[SeverityLevel.Warning,SeverityLevel.Error,SeverityLevel.Critical]],
+        ["ERROR",[SeverityLevel.Error,SeverityLevel.Critical]]
+      ]);
+     trace(message: string): void {
+      this.logMessage(message,SeverityLevel.Verbose);
     }
-
-    /**
-     * For logging information necessary to see in all envirnonments.
-     * @param message String of information to log.
-     */
-    info(message: string): void {
-        this.logMessage(message, SeverityLevel.Information);
+    info( message: string): void {
+      this.logMessage(message,SeverityLevel.Information);
     }
-
     warn(message: string): void {
-        this.logMessage(message, SeverityLevel.Warning);
+      this.logMessage(message,SeverityLevel.Warning);
     }
-
-    async error(message: string, error: Error): Promise<void> {
-        message = await this.createDetailedMsg(message, error);
-        this.logMessage(message, SeverityLevel.Error);
-        this.trackException(error, SeverityLevel.Error);
-    }
-
+   async error(message: string, error: Error): Promise<void> {
+      message=await this.createDetailedMsg(message, error);
+      this.logMessage(message,SeverityLevel.Error);
+      this.trackException(error,SeverityLevel.Error);
+       
+   }
     async critical(message: string, error: Error): Promise<void> {
-        message = await this.createDetailedMsg(message, error);
-        this.logMessage(message, SeverityLevel.Critical);
-        this.trackException(error, SeverityLevel.Critical);
+      message=await this.createDetailedMsg(message, error);
+      this.logMessage(message,SeverityLevel.Critical);
+      this.trackException(error,SeverityLevel.Critical);
     }
 
-    private async createDetailedMsg(message: string, error: Error): Promise<string> {
-        let errorDetails: string;
-        if (error) {
-            message = `${message} - ${error.message}`;
-            if (error instanceof BaseError) {
-                errorDetails = JSON.stringify(classToPlain(error));
-            }
+    private async createDetailedMsg(message:string,error:Error): Promise<string>{
+      let errorDetails:string;
+      if(error){
+        message=`${message} - ${error.message}`;
+        if(error instanceof BaseError){
+          errorDetails=JSON.stringify(classToPlain(error));
+          
         }
-        if (errorDetails) {
-            message = `message - ${message} , errordetails -${errorDetails}
+      }
+      if(errorDetails){
+        message=`message - ${message} , errordetails -${errorDetails}
         and error stack trace is ${error.stack}`;
-        } else {
-            message = `message - ${message} ,
+      }else{
+		  message=`message - ${message} ,
 		  and error stack trace is ${error.stack}`;
-        }
-        return message;
+	  }
+      return message;
     }
-
-    private async logMessage(message: string, severity: SeverityLevel): Promise<void> {
-        const isFilterRequired = await this.filterTrace(severity);
-        if (!isFilterRequired) {
-            if (this.env === "local") {
-                console.log(`${message}`);
-            } else {
-                this.client.trackTrace({
-                    message: `${message} `,
-                    severity,
-                    properties: {
-                        environment: this.env,
-                        APPINFO: "APPINFO",
-                    },
-                });
-            }
+    private async logMessage(message: string,severity:SeverityLevel): Promise<void>{
+      const isFilterRequired=await this.filterTrace(severity)
+      if(!isFilterRequired){
+        if(this.env==="local"){
+          console.log(`${message}`);
+        }else{
+            this.client.trackTrace({ 
+              message: `${message} `, 
+              severity,
+              properties: { 
+                "environment":this.env ,
+                "APPINFO":"APPINFO"
+              }
+            });
         }
+      }
+      
+  }
+
+   private async filterTrace (severity:SeverityLevel): Promise<boolean> {
+    let loglevel=process.env["LOG_LEVEL"];
+    if(!loglevel){
+      loglevel="ERROR";
     }
-
-    private async filterTrace(severity: SeverityLevel): Promise<boolean> {
-        let loglevel = process.env["LOG_LEVEL"];
-        if (!loglevel) {
-            loglevel = "ERROR";
-        }
-        if (loglevel && this.LogLevelMap.get(loglevel).includes(severity)) {
-            return false;
-        }
-
-        return true;
+    if (loglevel && this.LogLevelMap.get(loglevel).includes(severity)) {
+        return false;
     }
-
-    private async trackException(error: Error, severity: SeverityLevel): Promise<void> {
-        if (this.env !== "local") {
-            this.client.trackException({ exception: error, severity });
-        }
+  
+    return true;
+  }
+  private async trackException(error:Error,severity:SeverityLevel): Promise<void>{
+    if(this.env!=="local"){
+        this.client.trackException({exception:error,severity})
+      }
     }
 }
