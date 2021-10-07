448a3bb3d0eacc37bf8069bd175dddd5f10f51f8
Hannah Strachn <hannah.strachn@fairwaymc.com>
2021-10-07 15:49:28 -0500

    fix vulnerabilities in jest packages

--- code/mock/azure.mock.ts
+++ code/mock/azure.mock.ts
@@ -1,6 +1,6 @@
 import { Context, HttpRequest } from "@azure/functions";
 
-const logger = (d) => d;
+const logger = (d: any) => d;
 logger.info = jest.fn();
 logger.error = jest.fn();
 logger.warn = jest.fn();
@@ -19,7 +19,6 @@ export const ContextMock: Context = {
     traceContext: null,
 };
 
-//Update HttpReq obj to pass data for test cases
 export const HttpRequestMock: HttpRequest = {
     method: "GET",
     headers: null,
@@ -45,6 +44,7 @@ export const HttpRequestMockPost: HttpRequest = {
     url: null,
     body: { id: "" },
 };
+
 export const HttpRequestMockPostwithID: HttpRequest = {
     method: "POST",
     headers: null,
@@ -63,49 +63,6 @@ export const HttpRequestMockGet: HttpRequest = {
     body: null,
 };
 
-export const HttpRequestMockGetWithoutEmail = {
-    method: "GET",
-    headers: null,
-    params: null,
-    url: "/api/dashboard",
-    body: null,
-};
-
-export const HttpRequestMockInvalidLeads: HttpRequest = {
-    method: "POST",
-    headers: null,
-    params: null,
-    query: null,
-    url: null,
-    body: { loEmail: "" },
-};
-
-export const HttpRequestMockValidLeads: HttpRequest = {
-    method: "POST",
-    headers: null,
-    params: null,
-    query: null,
-    url: null,
-    body: { loEmail: "testUser@test.com" },
-};
-
-export const HttpRequestMockInvalidUpdtLeadInfo: HttpRequest = {
-    method: "POST",
-    headers: null,
-    params: null,
-    query: null,
-    url: null,
-    body: {},
-};
-export const HttpRequestMockValidUpdtLeadInfo: HttpRequest = {
-    method: "POST",
-    headers: null,
-    params: null,
-    query: null,
-    url: null,
-    body: { id: "12355" },
-};
-
 export const HttpRequestMockHealthCheck: HttpRequest = {
     method: "GET",
     headers: null,
@@ -113,22 +70,6 @@ export const HttpRequestMockHealthCheck: HttpRequest = {
     query: null,
     url: null,
     body: null,
-};
-
-export const HttpRequestMockSendEmail: HttpRequest = {
-    method: "POST",
-    headers: null,
-    params: null,
-    query: null,
-    url: null,
-    body: {
-        from: "no-reply@fairwaymc.com",
-        to: ["chandra.mandadi@fairwaymc.com"],
-        subject: "test",
-        text: "",
-        html: "<html>This is test</html>",
-        attachments: [],
-    },
 };
 
 export const HttpRequestMockPostUpdateLoGroup: HttpRequest = {
