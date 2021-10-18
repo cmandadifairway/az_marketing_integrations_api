a160f3b8e964648915ff0f9e535470251e5fc8c0
Hannah Strachn <hannah.strachn@fairwaymc.com>
2021-10-18 11:47:40 -0500

    Remove unnecessary methods and add forbidUnknownValues

--- code/shared/validators/customValidatorImpl.ts
+++ code/shared/validators/customValidatorImpl.ts
@@ -2,12 +2,10 @@ import { CustomValidator } from "./customValidator";
 import { ServiceBase } from "../services/serviceBase";
 import { validate } from "class-validator";
 import { plainToClass } from "class-transformer";
-import { TYPES } from "../inversify/types";
-import { LoanOfficerResponse } from "../model/loanOfficerResponse";
-import { LoanOfficerService } from "../services/loanOfficer/loanOfficerService";
 
 export class CustomValidatorImpl extends ServiceBase implements CustomValidator {
-    private readonly loanOfficerService = this.resolve<LoanOfficerService>(TYPES.LoanOfficerService);
+    private classTransformOptions = { excludeExtraneousValues: true };
+    private validatorOptions = { forbidUnknownValues: true };
 
     /**
      * Converts the object into the given class
@@ -16,7 +14,7 @@ export class CustomValidatorImpl extends ServiceBase implements CustomValidator 
      */
     convertToClass(classType: any, req: object): any {
         if (req === null || req === undefined) req = {};
-        return plainToClass(classType, req, { excludeExtraneousValues: true });
+        return plainToClass(classType, req, this.classTransformOptions);
     }
 
     /**
@@ -25,19 +23,7 @@ export class CustomValidatorImpl extends ServiceBase implements CustomValidator 
      */
     async validate(req: object): Promise<string[]> {
         if (req === null || req === undefined) req = {};
-        const errors = await validate(req);
-        return errors.map(({ constraints }) => constraints[Object.keys(constraints)[0]]);
-    }
-
-    /**
-     * Validates the object against the given schema
-     * @param schema - class with validation attributes
-     * @param req - request object. HttpRequest.query or HttpRequest.body
-     */
-    async convertAndValidate(schema: any, req: object): Promise<string[]> {
-        if (req === null || req === undefined) req = {};
-        const request: object = plainToClass(schema, req, { excludeExtraneousValues: true });
-        const errors = await validate(request);
+        const errors = await validate(req, this.validatorOptions);
         return errors.map(({ constraints }) => constraints[Object.keys(constraints)[0]]);
     }
 
@@ -47,43 +33,6 @@ export class CustomValidatorImpl extends ServiceBase implements CustomValidator 
                 const givenDate = new Date(date);
                 const today = new Date(Date.now());
                 return givenDate.getTime() > today.getTime();
-            }
-        } catch (error) {
-            return false;
-        }
-        return false;
-    }
-
-    public async isValidUser(loEmail: string): Promise<boolean> {
-        let loanOfficerResponse: LoanOfficerResponse;
-        try {
-            loanOfficerResponse = await this.loanOfficerService.getLoanOfficer(loEmail);
-            return loanOfficerResponse?.data?._id === loEmail;
-        } catch (error) {
-            return false;
-        }
-    }
-
-    public isMinBeforeMaxDate(minDate: string, maxDate: string): boolean {
-        try {
-            if (!isNaN(Date.parse(minDate))) {
-                const min = new Date(minDate);
-                const max = new Date(maxDate);
-                return max.getTime() > min.getTime();
-            }
-        } catch (error) {
-            return false;
-        }
-        return false;
-    }
-
-    public isDatesWeekApart(minDate: string, maxDate: string): boolean {
-        try {
-            if (!isNaN(Date.parse(minDate))) {
-                const min = new Date(minDate);
-                const max = new Date(maxDate);
-                const diffInMs = Math.abs(max.getTime() - min.getTime());
-                return diffInMs / (1000 * 60 * 60 * 24) < 7;
             }
         } catch (error) {
             return false;
